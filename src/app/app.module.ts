db7278de780d72e4fc1cf7b74d0c6f6863b6f177
dcaptain1 <gushol-0@student.ltu.se>
2023-04-14 10:12:01 +0200

    Pages work and redirects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { ButtonComponent } from './components/button/button.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 
 const appRoute: Routes= [
+  //{path: "", component: HomeComponent},  //bad version av skicka till home
+  {path: "", redirectTo: "Home", pathMatch: "full"}, // tom url skickar dig till home.
   {path: "Home", component: HomeComponent},
-  {path: "About", component: AboutComponent}
+  {path: "About", component: AboutComponent},
+  {path: "Contant", component: ContactComponent}
 ]
 
 
@@ -22,7 +26,8 @@ const appRoute: Routes= [
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
