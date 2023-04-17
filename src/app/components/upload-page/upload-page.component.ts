c8cd5940674290ffd6ec1d91a0837f1f5c6e60d3
Ludrnn-8 <ludrnn-8@student.ltu.se>
2023-04-17 16:38:57 +0200

    commit

--- src/app/components/upload-page/upload-page.component.ts
+++ src/app/components/upload-page/upload-page.component.ts
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload-page.component.css']
 })
 export class UploadPageComponent implements OnInit {
-  categories = [
-    { id: 1, name: 'Development'},
-    { id: 2, name: 'Art'},
-    { id: 3, name: 'Languages'},      
-  ];
+  categories =[
+    { id: 1, name: 'Entertainment' },
+    { id: 2, name: 'News' },
+    { id: 3, name: 'Politics' },
+    { id: 4, name: 'Sports' },
+    { id: 5, name: 'Culture' },
+    { id: 6, name: 'Economy' },
+    { id: 7, name: 'Nature' },
+];
 
   constructor () {}
 
