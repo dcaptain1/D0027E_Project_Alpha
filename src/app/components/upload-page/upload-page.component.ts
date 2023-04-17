055ffea06afc5470797242199c53474af0ee91fa
Ludrnn-8 <ludrnn-8@student.ltu.se>
2023-04-17 08:53:42 +0200

    Commit

--- src/app/components/upload-page/upload-page.component.ts
+++ src/app/components/upload-page/upload-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-upload-page',
   templateUrl: './upload-page.component.html',
   styleUrls: ['./upload-page.component.css']
 })
-export class UploadPageComponent {
+export class UploadPageComponent implements OnInit {
+  categories = [
+    { id: 1, name: 'Development'},
+    { id: 2, name: 'Art'},
+    { id: 3, name: 'Languages'},      
+  ];
 
+  constructor () {}
+
+  ngOnInit() {
+  }
 }
