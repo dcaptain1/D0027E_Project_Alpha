ec586958819ac7f4d258d6efa4fd4267cd795139
dcaptain1 <gushol-0@student.ltu.se>
2023-04-17 13:37:17 +0200

    Riktigt coolt header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
