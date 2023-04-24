c6c1fed90417a8f05ccbc7db64492969bd79361a
dcaptain1 <gushol-0@student.ltu.se>
2023-04-24 13:24:48 +0200

    test

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Employee {
+
+  private Long id;
+  private String name;
+  private String email;
+  private String jobTitle;
+  private String phone;
+  private String imageUrl;
+  private String employeeCode;
+
+
+
+}
