6bee9b241386cc922ad3dd07e822b2e905971550
dcaptain1 <gushol-0@student.ltu.se>
2023-05-02 11:56:24 +0200

    skicka

--- src/app/components/upload-page/upload-page.component.ts
+++ src/app/components/upload-page/upload-page.component.ts
@@ -39,14 +39,20 @@ export class UploadPageComponent implements OnInit {
   //Gets called when the user clicks on submit to upload the image
   onUpload() {
     console.log(this.selectedFile);
-    
+
+
     //FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
     const uploadImageData = new FormData();
     uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
-  
+
+
+    console.log("hit kommer den....")
     //Make a call to the Spring Boot Application to save the image
     this.httpClient.post('http://localhost:8090/image/upload', uploadImageData, { observe: 'response' })
+
       .subscribe((response) => {
+        console.log("hit kommer den inte")
+
         if (response.status === 200) {
           this.message = 'Image uploaded successfully';
         } else {
